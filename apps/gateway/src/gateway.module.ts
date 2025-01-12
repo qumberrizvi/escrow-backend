13dc34d946627e2ee37fc68ca80c9113257f3d6a
Qumber Rizvi <hey@qumberrizvi.com>
2025-01-12 20:11:02 +0000

    Refactor gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -9,32 +9,23 @@ import {
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GatewayResolver } from './gateway.resolver';
-import { AuthModule } from './auth/auth.module';
-import { EscrowModule } from './escrow/escrow.module';
-import { MediaModule } from './media/media.module';
-import { NotificationModule } from './notification/notification.module';
-import { OrchestratorModule } from './orchestrator/orchestrator.module';
-import { PaymentModule } from './payment/payment.module';
-import { PropertyModule } from './property/property.module';
-import { UserModule } from './user/user.module';
+import { ServiceResolverModule } from './modules/service-resolver.module';
 
 @Module({
   imports: [
     CommonModule,
+    ServiceResolverModule,
+
+    /* FIXME: See if this can be moved to service-resolver.module
+     *  without affecting gateway.resolver */
     ClientsModule.register(...Object.values(MicroServiceClient)),
+
+    // TODO: Move to libs/graphql
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
       sortSchema: true,
     }),
-    AuthModule,
-    EscrowModule,
-    MediaModule,
-    NotificationModule,
-    OrchestratorModule,
-    PaymentModule,
-    PropertyModule,
-    UserModule,
   ],
   controllers: [GatewayController],
   providers: [GatewayService, GatewayResolver],
