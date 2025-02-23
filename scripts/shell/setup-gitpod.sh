ac3de02741150d73f3d3768c652c6957fcea535a
Qumber Rizvi <qumberrizvi@ymail.com>
2025-02-23 17:31:44 +0000

    Gitpod related setup

---
+++ scripts/shell/setup-gitpod.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Running setup.sh..."
+bash ./setup.sh
+
+# Install Redis if not already installed
+if ! command -v redis-server &> /dev/null; then
+  echo "Installing Redis..."
+  sudo apt-get update && sudo apt-get install -y redis-server
+else
+  echo "Redis is already installed."
+fi
